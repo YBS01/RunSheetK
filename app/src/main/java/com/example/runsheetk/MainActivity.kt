78f76eaedbc450008388d8f8e7e7268a6bedc2cd
YBS01 <103018325+YBS01@users.noreply.github.com>
2024-04-10 22:10:18 -0500

    dB_Working

--- app/src/main/java/com/example/runsheetk/MainActivity.kt
+++ app/src/main/java/com/example/runsheetk/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.runsheetk
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.runsheetk.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private lateinit var firebaseRef : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseRef = FirebaseDatabase.getInstance().getReference("test")
+
+
+        binding.tvSendData.setOnClickListener {
+
+            firebaseRef.setValue("what hath God wrought").
+            addOnCompleteListener{
+                Toast.makeText(this, "Data added successfully", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
